11c9e53c46ec789950a93cbae87a0666dc1f8a53
bobolle <botdahlstrom@gmail.com>
2025-04-15 14:21:24 +0200

    feat: send moisture and photoresistor data

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "pico/stdlib.h"
 #include "wifi.h"
 #include "mqtt.h"
+#include "photoresistor.h"
+#include "moisture.h"
 
 int main(void)
 {
@@ -13,11 +15,20 @@ int main(void)
 
     mqtt_init();
     mqtt_connect();
+    photoresistor_init(27);
+    moisture_init(26, 16);
 
     while (1)
     {
-        publish_read("light", 30);
-        sleep_ms(5000);
+        uint16_t photoresistor_value = photoresistor_read(27);
+        printf("photoresistor value: %u\n", photoresistor_value);
+        publish_read("light", photoresistor_value);
+        
+        uint16_t moisture_value = moisture_read(26, 16);
+        printf("moisture value: %u\n", moisture_value);
+        publish_read("moisture", moisture_value);
+
+        sleep_ms(10000);
     }
 
     return 0;
